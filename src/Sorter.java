3cab0c0a9dc2c064d87e4b0cdfb97caf6791e9f6
Osama <os.albassam@gmail.com>
2023-09-20 16:46:01 +0300

    Adding selectionSort(int[] arr) method

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,2 +1,20 @@
 public class Sorter {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in the unsorted portion of the array
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the minimum element with the first element in the unsorted portion
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
 }
